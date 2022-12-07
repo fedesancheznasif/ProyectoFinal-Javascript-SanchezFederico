20014756d94c81dfc2f0f7420068748b46b20b93
Federico Joel Sanchez Nasif <fedesancheznasif@gmailcom>
2022-12-07 19:18:05 -0300

    Proyecto Final Javascript Listo

---
+++ scripts/filtradoYOrden/filtradoYOrden.js
@@ -0,0 +1,131 @@
+import { arrayJuegosInicial} from "../baseDeDatos/baseDatos.js";
+import {crearDivsJuegos} from "../funcionesGaleriaJuegos/funcionesGaleria.js";
+import {actualizarBotonCompra} from "../funcionesCarrito/funcionesCarrito.js";
+let arrayJuegos=arrayJuegosInicial;
+/*
+    Funcion que elimina todos los divs presentes de juegos en la galeria, los que esten en
+    el "arrayJuegos".
+*/
+function eliminarDivsJuegos(){
+    for(let juegoBuscado of arrayJuegos){
+        let divBuscado=document.getElementById(juegoBuscado.id);
+        divBuscado.remove();
+    }
+}
+/*
+    Agrega el evento de enviar al formulario que filtra los productos por genero.
+*/
+function agregarEventoGenero(carrito){
+    let boton=document.getElementById("filtrar");
+    boton.onclick=(e)=>{
+        let formulario=document.getElementById("formulario");
+        let form= new FormData(formulario);
+        e.preventDefault();
+        let generoSeleccionado=(form.get("genero"));
+        if(generoSeleccionado!=null){
+            let galeria=document.querySelector("#galeria");
+            galeria.style.opacity="0";
+            setTimeout(filtrarPorGenero,800,generoSeleccionado,carrito);
+        }
+    }
+}
+/*
+    Realiza la accion de crear un nuevo array solo con juegos del genero recibido por parametro
+*/
+function filtrarPorGenero(genero,carrito){
+    let nuevoArray=arrayJuegosInicial.filter(juego=>juego.genero==genero);
+    eliminarDivsJuegos();
+    arrayJuegos=nuevoArray;
+    crearDivsJuegos(nuevoArray,carrito);
+    actualizarTodosLosBotones(nuevoArray,carrito);
+}
+/*
+    Agrega el evento para reestablecer los juegos sin ningun filtro.
+*/
+function agregarEventoReestablecer(carrito){
+    let botonReset=document.getElementById("reestablecer");
+    botonReset.onclick=()=>{
+        let galeria=document.querySelector("#galeria");
+        galeria.style.opacity="0";
+        setTimeout(function(){
+            eliminarDivsJuegos();
+            arrayJuegos=arrayJuegosInicial;
+            crearDivsJuegos(arrayJuegosInicial,carrito);
+            actualizarTodosLosBotones(arrayJuegosInicial,carrito);
+        },700);
+    }
+}
+/*
+    Actualiza todos los botones de compra segun el carrito, o sea que va a decir "AGREGAR AL CARRITO"
+    si no está, o "EN EL CARRITO" si está.
+*/
+function actualizarTodosLosBotones(cualquierArrayJuegos,carrito){
+    for (let juego of cualquierArrayJuegos){
+        actualizarBotonCompra(juego,carrito);
+    }
+
+}
+/*
+    Agrega al boton de ordenar, el evento correspondiente para que pueda realizar dicha accion.
+*/
+function agregarEventoOrdenar(carrito){
+    let botonOrdenar=document.getElementById("ordenar");
+    botonOrdenar.onclick=(e)=>{
+        e.preventDefault();
+        let galeria=document.querySelector("#galeria");
+        galeria.style.opacity="0";
+        let orden=document.getElementById("select-orden").value;
+       setTimeout(function(){
+        switch(orden){
+            case "Menor a mayor precio":
+                ordenarMenorAMayor(carrito);
+                break;
+            case "Mayor a menor precio":
+                ordenarMayorAMenor(carrito);
+                break;
+            case "Alfabéticamente":
+                ordenarAlfabeticamente(carrito);
+                break;
+        }
+       },800);
+    }
+}
+/*
+    Utiliza arrayJuegos para ordenar por precio los juegos , de MENOR a MAYOR precio.
+*/
+function ordenarMenorAMayor(carrito){
+    let nuevoArray=arrayJuegos;
+    eliminarDivsJuegos();
+    nuevoArray.sort((a,b)=>a.precio- b.precio);
+    crearDivsJuegos(nuevoArray,carrito);
+    actualizarTodosLosBotones(nuevoArray,carrito);
+
+}
+/*
+    Utiliza arrayJuegos para ordenar por precio los juegos , de MAYOR a MENOR precio.
+*/
+function ordenarMayorAMenor(carrito){
+    let nuevoArray=arrayJuegos;
+    eliminarDivsJuegos();
+    nuevoArray.sort((a,b)=>b.precio-a.precio);
+    crearDivsJuegos(nuevoArray,carrito);
+    actualizarTodosLosBotones(nuevoArray,carrito);
+}
+/*
+    Funcion auxiliar para ordenar por nombre los juegos.
+*/
+function SortArray(a,b){
+    return a.nombre.localeCompare(b.nombre);
+}
+/*
+    Utiliza arrayJuegos para ordenar por orden alfabetico los juegos.
+*/
+function ordenarAlfabeticamente(carrito){
+    let nuevoArray=arrayJuegos;
+    eliminarDivsJuegos();
+    nuevoArray=nuevoArray.sort(SortArray);
+    crearDivsJuegos(nuevoArray,carrito);
+
+    actualizarTodosLosBotones(nuevoArray,carrito);
+}
+export {agregarEventoGenero,agregarEventoReestablecer,agregarEventoOrdenar,ordenarAlfabeticamente,arrayJuegos};
---
+++ scripts/main/main.js
@@ -0,0 +1,35 @@
+import { arrayJuegosInicial } from "../baseDeDatos/baseDatos.js";
+import { setearEstadoCarrito,crearProductoEnCarrito ,actualizarBotonCompra,actualizarSubtotal} from "../funcionesCarrito/funcionesCarrito.js";
+import {crearDivsJuegos} from "../funcionesGaleriaJuegos/funcionesGaleria.js";
+import {agregarEventoGenero, arrayJuegos,agregarEventoReestablecer, agregarEventoOrdenar, ordenarAlfabeticamente} from "../filtradoYOrden/filtradoYOrden.js";
+let carrito=[];
+let carritoEnLS=localStorage.getItem('Carrito');
+if(carritoEnLS){
+    carrito=JSON.parse(carritoEnLS);
+}
+function agregarReproduccionAutomaticaVideo(){
+    let body=document.getElementById("body");
+    let video=document.getElementById("video");
+    video.play();
+    body.onscroll=()=>{
+        if(window.scrollY<150){
+            video.play();
+        }
+    }
+}
+
+
+agregarReproduccionAutomaticaVideo();
+setearEstadoCarrito(carrito,arrayJuegos);
+crearDivsJuegos(arrayJuegos,carrito);
+agregarEventoGenero(carrito);
+agregarEventoReestablecer(carrito);
+agregarEventoOrdenar(carrito);
+
+
+
+for(let producto of carrito){
+    crearProductoEnCarrito(producto,carrito);
+    actualizarBotonCompra(producto,carrito);
+    actualizarSubtotal(carrito);
+}
